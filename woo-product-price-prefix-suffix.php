d3012aebf00122db65b672db47c3e8966b390ed1
DESKTOP-UB5T4UL\admin <ali.wpsprintplan@gmai.com>
2023-09-19 10:49:05 +0530

    Show "Settings saved successfully" on Save Change Menu  name

--- woo-product-price-prefix-suffix.php
+++ woo-product-price-prefix-suffix.php
@@ -22,6 +22,8 @@ function wp_add_prefix_suffix(){
 
 // Store the custom prefix and suffix in WordPress options
 function wppps_callback(){
+    $confirmation_message = '';
+
     if (isset($_POST['submit'])) {
         // Get the values from the form fields
         $prefix = sanitize_text_field($_POST['prefix']);
@@ -30,28 +32,43 @@ function wppps_callback(){
         // Save the values in WordPress options
         update_option('custom_price_prefix', $prefix);
         update_option('custom_price_suffix', $suffix);
+
+        // Set the confirmation message
+        $confirmation_message = 'Settings saved successfully!';
     }
 
     // Retrieve the custom prefix and suffix from options
     $custom_prefix = get_option('custom_price_prefix', '');
     $custom_suffix = get_option('custom_price_suffix', '');
     
-    // Output the form with the stored values
+    // Output the form with the stored values and confirmation message
     ?>
     <div class="wrap">
-       
+        <?php if (!empty($confirmation_message)) : ?>
+            <div class="notice notice-success is-dismissible">
+                <p><?php echo esc_html($confirmation_message); ?></p>
+            </div>
+        <?php endif; ?>
+
         <form method="post">
-            <label for="prefix">Prefix:</label>
-            <input type="text" id="prefix" name="prefix" value="<?php echo esc_attr($custom_prefix); ?>"><br><br>
-
-            <label for="suffix">Suffix:</label>
-            <input type="text" id="suffix" name="suffix" value="<?php echo esc_attr($custom_suffix); ?>"><br><br>
-
-            <input type="submit" name="submit" value="Submit">
+            <table>
+                <tr>
+                    <td><label for="prefix">Prefix:</label></td>
+                    <td><input type="text" id="prefix" name="prefix" value="<?php echo esc_attr($custom_prefix); ?>"></td>
+                </tr>
+                <tr>
+                    <td><label for="suffix">Suffix:</label></td>
+                    <td><input type="text" id="suffix" name="suffix" value="<?php echo esc_attr($custom_suffix); ?>"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" name="submit" value="Submit"></td>
+                </tr>
+            </table>
         </form>
     </div>
     <?php
 }
+
 
 // Modify WooCommerce prices using the stored prefix and suffix
 add_filter('woocommerce_get_price_suffix', 'geekerhub_add_price_suffix', 99, 4);
